113e7ced232e9d9069a2f2b25f2d352b311c278b
Harsh Solanki <harshsolanki5575@gmai.com>
2026-03-27 00:30:56 +0530

    regestration fixed

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -1,8 +1,4 @@
 function errorMiddleware(err, req, res, next) {
-  const statusCode = err?.statusCode || 500;
-  if (statusCode >= 500) {
-    console.error('[Error]', err);
-  }
   if (err && err.name === 'MulterError') {
     const message =
       err.code === 'LIMIT_FILE_SIZE'
@@ -30,6 +26,7 @@ function errorMiddleware(err, req, res, next) {
     return;
   }
 
+  const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal server error';
 
   res.status(statusCode).json({
